8033e75d6cd76d9a5baacd72efa47e793ed04db0
Mati <maticzacza@gmail.com>
2024-01-02 20:13:36 +0100

    update

--- index.js
+++ index.js
@@ -1,23 +1,7 @@
-const express = require('express');
-const cors = require('cors');
 const db = require('./db/db');
-const { readdirSync } = require('fs');
-
-// display sample Hello World page under '/'
-
-const app = express();
-require('dotenv').config();
+const app = require('./app');
 
 const PORT = process.env.PORT || 5000;
-
-// middlewares
-app.use(express.json());
-app.use(cors());
-
-// routes
-readdirSync('./routes').map((route) =>
-  app.use('/api/v1', require(`./routes/${route}`))
-);
 
 const server = () => {
   db();
@@ -25,12 +9,5 @@ const server = () => {
     console.log(`Server is running on port ${PORT}`);
   });
 };
-
-app.use((req, res, next) => {
-  res.status(404).json({
-    success: false,
-    message: 'Page not found',
-  });
-});
 
 server();
